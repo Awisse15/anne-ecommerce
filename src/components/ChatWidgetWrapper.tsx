8c25181cbc51ca8f39d77791ed10c0b74b1df429
Anne <echanes.anne.louisse@gmail.com>
2026-01-16 13:57:33 +0800

    update layout

--- src/components/ChatWidgetWrapper.tsx
+++ src/components/ChatWidgetWrapper.tsx
@@ -6,11 +6,13 @@ export default function ChatWidgetWrapper() {
   return (
     <ChatWidget
       token="2a33ce6d-53fb-4c25-b9f6-3978ea9aedd5"
-      inbox="cbcad038-825f-4da5-a2d4-4f7740063589"
+      accountId="2a33ce6d-53fb-4c25-b9f6-3978ea9aedd5"
+      baseUrl="https://papercups-latest-yob9.onrender.com"
       title="Welcome to Crafted by Louisse"
       subtitle="Ask us anything about our flowers! 💐"
       primaryColor="#f78da7"
-      baseUrl="https://papercups-latest-yob9.onrender.com"
+      greeting="Hi there! How can we help you today?"
+      newMessagePlaceholder="Start typing..."
       showAgentAvailability={false}
       requireEmailUpfront={false}
     />
